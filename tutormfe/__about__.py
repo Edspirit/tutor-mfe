852511656bb2ac83d862181c21d0a53f1fceed7e
Régis Behmo <regis@behmo.com>
2023-05-26 16:28:10 +0200

    chore: handle nightly version numbers
    Here, we make it possible to add a "-nightly" suffix to the package
    version. This suffix will find its way to the Docker image tags. Thus,
    the nightly branch will have different image tags. This will resolve
    some confusion, as image tags are currently identical in nightly and
    master.

--- tutormfe/__about__.py
+++ tutormfe/__about__.py
@@ -1,1 +1,8 @@
-__version__ = "16.1.3"
+__version__ = "15.0.8"
+
+
+# Handle version suffix for nightly, just like tutor core.
+__version_suffix__ = ""
+
+if __version_suffix__:
+    __version__ += "-" + __version_suffix__
